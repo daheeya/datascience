d96035eaa387df38705011772c7bece27fdf9553
다히 <102225999+daheeya@users.noreply.github.com>
2024-03-12 20:47:15 +0900

    ch13/quiz4
    미완성..

--- AppUI.java
+++ AppUI.java
@@ -1,33 +1,8 @@
-package ch13.quiz3;
+package ch13.quiz4;
 
 public class AppUI {
     public static void main(String[] args) {
-        Person james = new Person();
-        Person tomas = new Person();
-        Vehicle bus = new Vehicle();
-        Vehicle subway = new Vehicle();
-        bus.setName("bus");
-        bus.setPrice(1000);
-
-        subway.setName("subway");
-        subway.setPrice(1500);
-
-        james.setName("james");
-        james.setMoney(5000);
-
-
-        tomas.setName("tomas");
-        tomas.setMoney(4000);
-
-
-        james.takeVehicle(bus);
-        james.takeVehicle(bus);
-        bus.printInfo();
-        james.printInfo();
-        james.takeVehicle(subway);
-        james.printInfo();
+        Cafe cafe = new Cafe();
 
     }
-
-
 }
---
+++ Menu.java
@@ -0,0 +1,12 @@
+package ch13.quiz4;
+
+public class Menu {
+    String name;
+    int price;
+
+    public void setMenu(String name, int price){
+
+        this.name = name;
+        this.price = price;
+    }
+}
